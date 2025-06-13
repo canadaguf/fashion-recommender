eecab8b95169344cfdd0d31d7f1ffeb112528a89
canadaguf <ilyaiono@gmail.com>
2025-06-13 09:29:00 +0300

    backendend port enabled

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,8 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from recommender import app as recommender_app
 
+import os
+
 if __name__ == "__main__":
-    recommender_app.run()
+    port = int(os.environ.get("PORT", 10000))  # Render использует PORT
+    recommender_app.run(host="0.0.0.0", port=port)
